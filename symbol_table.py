1b5b0ffbfa1a03a76f8242df3d2f34df1d2478c5
sheldon05 <jesussantoscapote@gmail.com>
2023-01-02 23:48:58 -0500

    Symbol Table Finished

---
+++ symbol_table.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+class DataType(Enum):
+    INT = 1
+    FLOAT = 2
+    STRING = 3
+    GRAPH = 4
+    DIGRAPH = 5
+    PSEUDOGRAPH = 6
+
+
+class Symbol():
+    def __init__(self, id, data_type, value) :
+        self.id = id
+        self.data_type = data_type
+        self.value = value
+
+
+class SymbolTable():
+    def __init__(self, symbols = {}) :
+        self.symbols = symbols
+
+
+    def add(self, symbol):
+        self.symbols[symbol.id] = symbol
+    
+
+    def get(self, id):
+        if not id in self.symbols:
+            print('Error: variable ', id, ' not defined.')
+
+        return self.symbols[id]
+
+
+    def update(self, symbol) :
+        if not symbol.id in self.symbols :
+            print('Error: variable ', symbol.id, ' not defined.')
+        else :
+            self.symbols[symbol.id] = symbol
