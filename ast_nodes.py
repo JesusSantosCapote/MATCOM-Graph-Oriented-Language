fac7ca742a1d97e5d90f3c52a439dd8677b431e7
KennyVillalobos <kenny.villalobos.kuco@gmail.com>
2023-01-03 03:41:34 -0500

    Main implemented. Assign inst implemented. Not full tested yet.

--- ast_nodes.py
+++ ast_nodes.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from symbol_table import *
 
 
 class Node(ABC):
     @abstractmethod
-    def evaluate(self):
+    def evaluate(self, st):
         pass       
 
 
@@ -11,9 +12,9 @@ class Instructions(Node):
     def __init__(self, node_list) -> None:
         self.node_list = node_list
 
-    def evaluate(self):
+    def evaluate(self, st):
         for instruction in self.node_list:
-            instruction.evaluate
+            instruction.evaluate(st)
 
 
 class Plot(Node) :
@@ -21,18 +22,39 @@ class Plot(Node) :
     def __init__(self,  graph) :
         self.graph = graph
 
-    def evaluate(self): #TODO not implemented
+    def evaluate(self, st): #TODO not implemented
         pass
 
 
 class Assign(Node) :
-    '''
-        Esta clase representa la instrucción de asignación de variables
-        Recibe como parámetro el identificador a asignar y el valor que será asignado.
-    '''
 
     def __init__(self, id, graph_type, vertex, edges_expression) :
         self.id = id
         self.graph_type = graph_type
         self.vertex = vertex
         self.edges_expression = edges_expression
+
+    def evaluate(self, symbol_table : SymbolTable):
+        
+        for edge in self.edges_expression: #TODO:This need to be fixed. For the iterations checking multiple-edges
+            if edge[0] >= self.vertex or edge[0] < 0 or edge[1] >= self.vertex or edge[1] < 0:
+                raise Exception("Edge non-existent")
+            if self.graph_type != "PSEUDOGRAPH":
+                if edge[0] == edge[1]:
+                    raise Exception(f"{self.graph_type} can not have loop edges")
+            if self.graph_type != "MULTIGRAPH":
+                for remain_edge in range(edge,len(self.edges_expression)):
+                    if remain_edge[0] == edge[0] and remain_edge[1] == edge[1]:
+                        raise Exception(f"{self.graph_type} can not have multiple edges")
+            if self.graph_type != "DIGRAPH":
+                for remain_edge in range(edge,len(self.edges_expression)):
+                    if remain_edge[0] == edge[1] and remain_edge[1] == edge[0]:
+                        raise Exception(f"{self.graph_type} can not have multiple edges")
+            
+
+        if id in symbol_table.symbols.keys():
+            symbol_table.update(Symbol(self.id,self.graph_type,[self.vertex, self.edges_expression])) 
+        else:
+            symbol_table.add(Symbol(self.id,self.graph_type,[self.vertex, self.edges_expression])) 
+
+
--- dsl_name.py
+++ dsl_name.py
@@ -1,17 +1,11 @@
 import sys
-from scanner import Scanner
-
+from parser_rules import parser
+from symbol_table import *
 class DSL:
-
-    had_error = False
 
     def main(self, file_name = None):
         if file_name != None:
             self.run_file(file_name)
-
-        else:
-            self.run_prompt()
-
 
     def run_file(self, file_name):
         code = ""
@@ -20,39 +14,15 @@ class DSL:
         file.close()
 
         self.run(code)
-        
-        if self.had_error: print("The code had errors") #TODO: Think a better form to do this
-
-
-    def run_prompt(self):
-        while True:
-            try:
-                line = input(">>> ")
-            except EOFError:
-                break
-            self.run(line)
-            self.had_error = False
-
     
-    def run(source):
-        scanner = Scanner(source)
-        tokens = scanner.get_tokens()
+    def run(self, source):
+        program = parser.parse(source)
+        st = SymbolTable()
+        program.evaluate(st)
+        print(st.symbols)
 
-        #for now only print
-        for tok in tokens:
-            print(tok)
-
-
-    @staticmethod
-    def report(line_number, where, message): #TODO check if this work
-        print("[line " + line_number + "] Error" + where + ": " + message)
-        DSL.had_error = True
+my_DSL = DSL()
+my_DSL.main("source.txt")
 
 
-    @staticmethod
-    def error(line_number, message):
-        DSL.report(line_number, "", message)
-
-
-    
 
