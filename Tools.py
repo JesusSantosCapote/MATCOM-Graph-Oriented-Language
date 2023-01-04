16baf202b1d6f93c3232066a14f59be0c79ff29a
sheldon05 <jesussantoscapote@gmail.com>
2023-01-04 17:44:45 -0500

    Intersection fixed

--- Tools.py
+++ Tools.py
@@ -27,7 +27,6 @@ def union(graph1, graph2):
     return nx.operators.compose(graph1, graph2)
 
 
-#TODO: intersection needs to be fixed
 def intersection(graph1, graph2):
     graph1_copy = graph1.copy()
     graph2_copy = graph2.copy()
@@ -97,33 +96,3 @@ Graph_Operations={
     'intersection' : intersection,
     'difference' : difference
 }
-
-
-
-a = nx.Graph([('pepe', 2), ('kuko', 'nosy')])
-a.add_nodes_from([(0, {'color':'red'}), (1, {'color':'blue'}), (2, {'age': 60})])
-a.add_edges_from([(0,1, {"weigth":5}), (0,2, {"code":5}), (0,0), (0,1)])
-
-print(a._node)
-print(a.edges)
-
-b = nx.Graph([('pepe', 2), ('kuko', 'nosy'), ('nosy', 1)])
-b.add_nodes_from([(0, {'color':'black'}), (1, {'audio':'stereo'}), (2, {'age': 50})])
-b.add_edges_from([(0,1,{"weigth":5, "code":123}), (2, 3,{"weigth":5}), (0,1)])
-
-print(b._node)
-print(b.edges)
-
-c = intersection(a, b)
-
-d = nx.Graph([(5,0), (7,2), (2,0), (4, 5)])
-e = nx.Graph([(2,0), (7,2)])
-
-print(d.nodes)
-print(e.nodes)
-
-c = difference(d, e)
-
-print(c._node)
-print(b.edges)
-nx.get
