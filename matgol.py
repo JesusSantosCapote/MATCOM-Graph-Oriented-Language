b592e4cb0626f1f6dde1c465b209b215521d5698
dfg-98 <dariofg98@gmail.com>
2023-01-06 18:51:16 -0500

    get source from cli args

--- matgol.py
+++ matgol.py
@@ -1,29 +1,39 @@
-import sys
-from parser_rules import parser
-from symbol_table import *
-class DSL:
+#!/usr/bin/env python3
 
-    def main(self, file_name = None):
+from optparse import OptionParser
+from parser_rules import parser
+from symbol_table import SymbolTable
+
+
+class DSL:
+    def main(self, file_name=None):
         if file_name != None:
             self.run_file(file_name)
 
     def run_file(self, file_name):
         code = ""
+
         with open(file_name) as file:
             code = file.read()
-        file.close()
 
         self.run(code)
-    
+
     def run(self, source):
         program = parser.parse(source)
         st = SymbolTable()
         program.evaluate(st)
         print(st.symbols)
 
-my_DSL = DSL()
-source = f"input_codes/{input()}.txt"
-my_DSL.main(source)
 
+if __name__ == "__main__":
+    usage = "matgol.py source"
+    parser = OptionParser(usage=usage)
 
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error("Incorrect number of arguments")
 
+    source = args[0]
+    print(source)
+    dsl = DSL()
+    dsl.main(source)
